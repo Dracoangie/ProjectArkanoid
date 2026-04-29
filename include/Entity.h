1b45af075cd9137c49e50ba187c1d37631392c7b
DevPatata <anavaro.sala@gmail.com>
2026-04-29 13:41:17 +0200

    Basic Setup

--- include/Entity.h
+++ include/Entity.h
@@ -21,14 +21,34 @@ struct Transform
 class Entity
 {
 protected:
+	/**
+	 * The texture of the entity, used for rendering. 
+	 * It is loaded in the start() method and destroyed in the destructor of the derived class.
+	 */
 	SDL_Texture* texture = nullptr;
 
 public:
+	/**
+	 * The transform component of the entity, containing its position and size.
+	 */
 	Transform transform;
 
 	virtual ~Entity() {}
+
+	/**
+	 * Is called once when the entity is added to the scene. 
+	 * It is used to load the texture and initialize any other resources needed by the entity.
+	 */
 	virtual void start(SDL_Renderer* renderer) = 0;
+
+	/**
+	 * Is called every frame to update the entity's state.
+	 */
 	virtual void update() = 0;
+
+	/**
+	 * Is called every frame to render the entity on the screen.
+	 */
 	virtual void render(SDL_Renderer* renderer) = 0;
 
 };
